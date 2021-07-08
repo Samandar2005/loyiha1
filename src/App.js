03fb781af2610ffdccfee71674740a1492a00aa2
Samandar2005 <samandarboriyev2005@gmail.com>
2021-07-08 17:55:36 +0500

    yengi nom

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Samandar react 1-dars
+          Samandar react 1-dars 2-ish
         </p>
         <a
           className="App-link"
