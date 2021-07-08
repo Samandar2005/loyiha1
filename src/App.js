fba0771c6b56fddc60c218fe684399ed0d6c8fdd
Samandar2005 <samandarboriyev2005@gmail.com>
2021-07-08 18:01:33 +0500

    ish 3 ga ozgardi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Samandar react 1-dars 2-ish
+          Samandar react 1-dars 3-ish
         </p>
         <a
           className="App-link"
