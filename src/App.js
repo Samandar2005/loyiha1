70b78502de5562d0491b78168e785ffdbc1ba086
Samandar2005 <samandarboriyev2005@gmail.com>
2021-07-15 13:00:55 +0500

    commentariya

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import DisplayComponent from './Components/DisplayComponent';
+import BinDisplayComponents from './Components/BinDisplayComponents';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Samandar react 1.2-dars 4it-ish
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+  const [time, setTime] = useState({ms:0, s:50, m:59, h:0});
+
+  const[interv, setInterv] = useState();
+  const[status, setStatus] = useState(0);
+
+  // const stop = () => {
+  //   clearInterval(interv)
+  //   setStatus(1);
+  // } ;
+
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  } ;
+
+  var upDatedMs = time.ms, upDatedS = time.s, upDatedM = time.m, upDatedH = time.h;
+
+  const run = () =>{
+    if(upDatedM === 60){
+      upDatedH++;
+      upDatedM = 0;
+    }
+    if(upDatedS === 60){
+      upDatedM++;
+      upDatedS = 0;
+    }
+    if(upDatedMs === 100){
+      upDatedS++;
+      upDatedMs = 0;
+    }
+
+    upDatedMs++;
+    return setTime({ms:upDatedMs, s:upDatedS, m:upDatedM, h:upDatedH});
+  }
+
+  const stop = () => {
+    clearInterval(interv)
+    setStatus(2);
+  } ;
+
+  const reset = () => {
+    clearInterval(interv)
+    setStatus(0);
+    setTime({ms:0, s:0, m:0, h:0});
+  } ;
+  const resume = () => start();
+
+
+  return(
+    <div className="main-section">
+      <div className="clock-holder">
+        <div className="stopwatch">
+          <DisplayComponent time={time} />
+          <BinDisplayComponents status={status} resume={resume} reset={reset} stop={stop} start={start} />
+        </div>
+
+      </div>
     </div>
   );
 }
